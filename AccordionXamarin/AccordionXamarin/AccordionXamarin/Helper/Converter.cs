8bc5be3414e8c4810170e7a3b17c67f052a93599
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-27 16:56:26 +0530

    XAMARIN-38880 - Sample and screenshot added.

---
+++ AccordionXamarin/AccordionXamarin/AccordionXamarin/Helper/Converter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AccordionXamarin.Helper
+{
+    #region ExpanderIconConverter
+    public class ExpanderIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return ImageSource.FromResource("AccordionXamarin.Images.ArrowDown.png");
+            else
+                return ImageSource.FromResource("AccordionXamarin.Images.ArrowUp.png");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion
+}
